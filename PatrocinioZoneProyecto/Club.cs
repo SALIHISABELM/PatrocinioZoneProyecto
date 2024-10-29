bedb2b0e992d6e06b5e24a8e5673462a0b94be55
Juanmadomingo <143461613+Juanmadomingo@users.noreply.github.com>
2024-10-29 15:31:31 -0300

    Update Club.cs

--- PatrocinioZoneProyecto/Club.cs
+++ PatrocinioZoneProyecto/Club.cs
@@ -6,17 +6,18 @@ using System.Threading.Tasks;
 
 namespace PatrocinioZoneProyecto
 {
-    public class Club
+    public class Club : Usuario
     {
         List<ZonaPatrocinio> zonas = new List<ZonaPatrocinio>();
+      
         public String Nombre { get; set; }
         public String Email { get; set; }
         //public String password { get; set; }
-        public int ClubId { get; set; }
         public Deporte Deporte { get; set; }
+        public Club(string id, string nombre, Deporte Deporte, string Email) : base(id)
 
 
-        //Posible Metodo publicarOferta()
-
+        //Posible Metodo publicarOferta(double precio):void
+        
     }
 }
