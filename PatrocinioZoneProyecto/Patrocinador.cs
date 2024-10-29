78686ed51cf65c5db56ae3582923b6e12e0c6d91
Juanmadomingo <143461613+Juanmadomingo@users.noreply.github.com>
2024-10-29 15:29:18 -0300

    Update Patrocinador.cs

--- PatrocinioZoneProyecto/Patrocinador.cs
+++ PatrocinioZoneProyecto/Patrocinador.cs
@@ -10,10 +10,17 @@ namespace PatrocinioZoneProyecto
     {
         List<Club> sponsoreados = new List<Club>();
 
+public Patrocinador(String id, String nombre ,double presupuesto ,String  Email ) : base(id)
+        {
+        Nombre = nombre;
+        Presupuesto = presupuesto;
+        Email = email;
+
+        }
         public String Nombre { get; set; }
         public String Email { get; set; }
         //public String password { get; set; }
-        public int PatrocinadorId { get; set; }
+        public string Id { get; set; }
         public double presupuesto {  get; set; }
 
 
